4ea8c7585f214e5546681b0c725b200937451b4d
dccjll <yiyeshu1015@vip.qq.com>
2018-01-07 23:35:41 +0800

    补充提交，待完善

--- app/src/main/java/com/paiai/android/test/XMTimeLineView.java
+++ app/src/main/java/com/paiai/android/test/XMTimeLineView.java
@@ -1,10 +1,10 @@
 package com.paiai.android.test;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
 import android.widget.HorizontalScrollView;
 
 /**
@@ -13,6 +13,18 @@ import android.widget.HorizontalScrollView;
 
 public class XMTimeLineView extends HorizontalScrollView {
 
+    private float lineSize = 0.5f;
+    private int lineColor = 0x9B9B9B;
+    private int tinySpace = 10;
+    private float longVerticalLineLength = 10;
+    private float shortVerticalLineLength = 5;
+    private int unitTimeLength = 10;//默认小单元格表示10分钟
+    private int unitSize = 6;//默认6个小单元格组成一个大单元格
+//    private float indicatorLineSize;
+//    private float indicatorRadius;
+//    private int indicatorLineColor;
+    private float timeFontSize = 9;
+    private int timeFontColor = 0x333333;
 
     public XMTimeLineView(Context context) {
         this(context, null);
@@ -20,7 +32,18 @@ public class XMTimeLineView extends HorizontalScrollView {
 
     public XMTimeLineView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.XMTimeLineView);
+        lineSize = DensityUtils.dp2px(getContext(), typedArray.getFloat(R.styleable.XMTimeLineView_lineSize, lineSize));
+        lineColor = typedArray.getColor(R.styleable.XMTimeLineView_lineColor, lineColor);
+        tinySpace = DensityUtils.dp2px(getContext(), typedArray.getInt(R.styleable.XMTimeLineView_tinySpace, tinySpace));
+        longVerticalLineLength = DensityUtils.dp2px(getContext(), typedArray.getFloat(R.styleable.XMTimeLineView_longVerticalLineLength, longVerticalLineLength));
+        shortVerticalLineLength = DensityUtils.dp2px(getContext(), typedArray.getFloat(R.styleable.XMTimeLineView_shortVerticalLineLength, shortVerticalLineLength));
+        unitTimeLength = DensityUtils.dp2px(getContext(), typedArray.getInt(R.styleable.XMTimeLineView_unitTimeLength, unitTimeLength));
+        unitSize = DensityUtils.dp2px(getContext(), typedArray.getInt(R.styleable.XMTimeLineView_unitSize, unitSize));
+//        indicatorLineSize = DensityUtils.dp2px(getContext(), typedArray.getFloat(R.styleable.XMTimeLineView_indicatorLineSize, indicatorLineSize));
+//        indicatorRadius = DensityUtils.dp2px(getContext(), typedArray.getFloat(R.styleable.XMTimeLineView_indicatorRadius, indicatorRadius));
+//        indicatorLineColor = typedArray.getColor(R.styleable.XMTimeLineView_indicatorLineColor, indicatorLineColor);
+        typedArray.recycle();
         setClickable(true);
     }
 
@@ -32,17 +55,5 @@ public class XMTimeLineView extends HorizontalScrollView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-//        if (event.getAction() == MotionEvent.ACTION_DOWN) {//按下
-//
-//        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-//
-//        } else if (event.getAction() == MotionEvent.ACTION_UP) {//抬起
-//
-//        }
-        return super.onTouchEvent(event);
     }
 }
